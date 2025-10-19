5155cac04b8c7ebf12cdaa6c87aa2c79a5a0e4f0
Kmoser1 <kmoser1@walsh.edu>
2025-10-19 19:04:39 -0400

    finished the logic for printing values that are 1 above the index number. Created step 4's function, called the function 3 times with 3 different inputs.

--- script.js
+++ script.js
@@ -12,29 +12,33 @@ for (let i = 0; i <= 5; i++) {
 // "My #1 favorite food is Ramen" (copy/paste for all items)
 // "My #2 favorite food is Sushi"
 // ...etc.
-
-
-
-
+for (let i = 0; i <= 5; i++) {
+  console.log("My #" + (i+1) + " favorite food is " + favFoods[i]);
+}
 
 // 4a. Create a function printFoodRecommendation(foodName) that prints out the following for the foodName provided
     // "Have you ever tried ____?"
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
-
-
-
+function printFoodRecommendation(foodName){
+  console.log("Have you tried " + favFoods[foodName]);
+  console.log("I always recommend " + favFoods[foodName] + " to friends.");
+  console.log("Trust me - " + favFoods[foodName] + " is delicious.");
+}
 
 // 4b. Call the function at least 3 times
 
-
+console.log(printFoodRecommendation(0));
+console.log(printFoodRecommendation(1));
+console.log(printFoodRecommendation(2));
 
 // Here's a list of 50 friends' favorite foods:
 let friendFavorites = [
-    "Pizza", "Sushi", "Pasta", "Falafel", "Burgers", "Ramen", "Pad Thai", "Curry", "Pho", "Nachos", "Gnocchi", "Donuts", "Steak", "Lasagna", "Biryani", "Tacos", "Croissant", "Churros", "Fried Rice", "Shawarma", "Miso Soup", "BBQ Ribs", "Hotpot", "Enchiladas", "Baklava", "Gyros", "Hummus", "Empanadas", "Pancakes", "Muffins", "Samosas", "Macarons", "Quiche", "Pierogi", "Arepas", "Okonomiyaki", "Ceviche", "Brisket", "Bao Buns", "Poutine", "Clam Chowder", "Fajitas", "Canelé", "Kimchi", "Tamales", "Omelette", "Biscuits", "Tempura", "Spring Rolls", "Crepes"
+  "Pizza", "Sushi", "Pasta", "Falafel", "Burgers", "Ramen", "Pad Thai", "Curry", "Pho", "Nachos", "Gnocchi", "Donuts", "Steak", "Lasagna", "Biryani", "Tacos", "Croissant", "Churros", "Fried Rice", "Shawarma", "Miso Soup", "BBQ Ribs", "Hotpot", "Enchiladas", "Baklava", "Gyros", "Hummus", "Empanadas", "Pancakes", "Muffins", "Samosas", "Macarons", "Quiche", "Pierogi", "Arepas", "Okonomiyaki", "Ceviche", "Brisket", "Bao Buns", "Poutine", "Clam Chowder", "Fajitas", "Canelé", "Kimchi", "Tamales", "Omelette", "Biscuits", "Tempura", "Spring Rolls", "Crepes"
   ];
 
 // 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
+// [I'm going to ignore the example, since Pizza has an "a" in the final letter of the string]
 
 
 
